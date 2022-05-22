58df05aa670bcd1faea7a8b89bf6d194d14e20a2
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-05-22 14:56:07 -0400

    Time: 3 ms (37.16%), Space: 45 MB (30.19%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public List<List<Integer>> permute(int[] nums) {
+      
+        List<List<Integer>> result = new ArrayList<>();
+        
+        recurse(result, new ArrayList<>(), nums);
+        
+        return result;
+    }
+    
+    private void recurse(List<List<Integer>> result, List<Integer> currList, int [] nums){
+        
+        if(currList.size() == nums.length){
+            result.add(new ArrayList<>(currList));
+            // return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){ 
+            if(!currList.contains(nums[i])){
+                currList.add(nums[i]);
+                recurse(result, currList, nums);
+                currList.remove(currList.size() - 1);
+            }     
+        }
+    }
+}
