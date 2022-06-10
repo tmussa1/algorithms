7b5420e3f197ff742041abb721f5d243b8cf1f3e
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-06-10 17:26:05 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (49.07%) - LeetHub

--- 796-rotate-string/796-rotate-string.java
+++ 796-rotate-string/796-rotate-string.java
@@ -7,35 +7,7 @@ class Solution {
             return false;
         }
         
-        if(s.length() == 1) {
-           if(!s.equals(goal)){
-             return false;
-           } else {
-             return true;
-           }
-        }
-        
         s = s + s;
-        
-//         StringBuilder target = new StringBuilder();
-//         target.append(s.substring(s.length() - 1));
-//         target.append(s.substring(0, s.length() - 1));
-        
-//         while(!s.equals(target.toString())){
-            
-//             if(goal.equals(target.toString())) {
-//                 return true;
-//             }
-            
-//             int length = target.length() - 1;
-            
-//             char c = target.charAt(length);
-            
-//             target.deleteCharAt(length);
-            
-//         }
-        
-//         System.out.println(target.toString());
         
         return s.indexOf(goal) != -1;
     }
