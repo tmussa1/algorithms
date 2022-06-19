0708956decb5564bcb98f4ffb574ad85e26cd5ab
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-06-18 23:04:52 -0400

    Time: 2877 ms (5.01%), Space: 42 MB (10.68%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+            
+        if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+         if(dividend == Integer.MIN_VALUE && divisor == Integer.MIN_VALUE) return 1;
+        if(dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
+        if(dividend == Integer.MAX_VALUE && divisor == 1) return Integer.MAX_VALUE;
+        
+        
+        if(dividend < 0 && divisor < 0){
+          return divideHelper(dividend, divisor);
+        }
+        
+        if(dividend < 0 || divisor < 0){
+          return -1 * divideHelper(dividend, divisor);
+        }
+        
+        return divideHelper(dividend, divisor);
+    }
+    
+    public int divideHelper(int dividend, int divisor) {
+        
+        if(dividend > 0){
+            dividend = -dividend;
+        }
+        
+        if(divisor > 0){;
+            divisor = -divisor;
+        }
+        
+        int quotient = 0;
+        
+        while(dividend - divisor <= 0){
+            quotient++;
+            dividend -= divisor;
+        }
+        
+        return quotient;
+    }
+}
