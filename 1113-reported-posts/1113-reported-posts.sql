715c5c9813aa64528c860b9f38733ee70abccd7d
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-06-28 10:34:02 -0400

    Time: 632 ms (52.09%), Space: 0B (100.00%) - LeetHub

---
+++ 1113-reported-posts/1113-reported-posts.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT extra AS report_reason, COUNT(DISTINCT post_id) AS report_count
+FROM Actions
+WHERE action_date = '2019-07-04'
+AND action = 'report'
+AND extra IS NOT NULL
+GROUP BY extra
+HAVING report_count > 0;
+
