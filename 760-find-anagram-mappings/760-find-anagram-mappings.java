ac93d1d20ca9e3c321d132089342ffe23197b1ab
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-06-30 20:52:22 -0400

    Time: 1 ms (99.34%), Space: 42.9 MB (35.98%) - LeetHub

---
+++ 760-find-anagram-mappings/760-find-anagram-mappings.java
@@ -0,0 +1,19 @@
+class Solution {
+    
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        
+        for(int i = 0; i < nums2.length; i++){
+            indexMap.put(nums2[i], i);
+        }
+        
+        int [] result = new int[nums1.length];
+        
+        for(int i = 0; i < nums1.length; i++){
+            result[i] = indexMap.get(nums1[i]);
+        }
+        
+        return result;
+    }
+}
