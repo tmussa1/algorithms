a15bd466047167b90889d701bf629827588a1d09
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-07-05 22:29:49 -0400

    Time: 26 ms (28.96%), Space: 43.9 MB (64.97%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -30,22 +30,20 @@ class Solution {
         
         if(root == null) return;
         
-        dfs(result, new ArrayList<>(), root, targetSum);
+        dfs(result, root, targetSum);
         traverse(result, root.left, targetSum);
         traverse(result, root.right, targetSum);
     }
     
-    private void dfs(int [] result, List<Integer> currList, TreeNode root, int targetSum){
+    private void dfs(int [] result, TreeNode root, int targetSum){
         
-       if(root == null) return;
+        if(root == null) return;
         
-       if(targetSum == root.val){
+        if(targetSum == root.val){
             result[0] += 1;
         }
         
-        currList.add(root.val);
-        
-        dfs(result, currList, root.left, targetSum - root.val);
-        dfs(result, currList, root.right, targetSum - root.val);
+        dfs(result, root.left, targetSum - root.val);
+        dfs(result, root.right, targetSum - root.val);
     }
 }
