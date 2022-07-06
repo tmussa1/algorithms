40eb7f1402939136bf773db1ce4b968f6d54e9ef
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-07-05 22:28:47 -0400

    Time: 97 ms (5.04%), Space: 68 MB (5.02%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -48,6 +48,6 @@ class Solution {
         dfs(result, currList, root.left, targetSum - root.val);
         dfs(result, currList, root.right, targetSum - root.val);
         
-        targetSum += root.val;
+        // targetSum += root.val;
     }
 }
