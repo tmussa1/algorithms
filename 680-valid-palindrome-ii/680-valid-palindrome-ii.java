c90aa42b2af79b72bb4f4b8bda39b9591495bf6a
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-07-18 14:03:28 -0400

    Time: 14 ms (30.47%), Space: 58.3 MB (7.94%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+    public boolean validPalindrome(String s) {
+        
+        int left = 0, right = s.length() - 1;
+        
+        char [] cArr = s.toCharArray();
+        
+        while(left < right){
+            
+            if(cArr[left] == cArr[right]){
+                left++;
+                right--;
+            } else {
+               return checkPalindrome(s, left + 1, right) || checkPalindrome(s, left, right - 1);
+            }
+        }
+        
+        
+        return true;
+    }
+    
+    private boolean checkPalindrome(String s, int left, int right){
+        
+        while(left < s.length() && right >= 0 && left < right){
+            
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    
+    
+}
