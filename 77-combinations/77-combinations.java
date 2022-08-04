05a761b5efab213142e35345e451e5eb2d9a2acf
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-08-03 20:13:51 -0400

    Time: 41 ms (42.65%), Space: 69.3 MB (5.85%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,28 +1,25 @@
 class Solution {
     
-    List<List<Integer>> output = new LinkedList();
-  int n;
-  int k;
-
-  public void backtrack(int first, LinkedList<Integer> curr) {
-    // if the combination is done
-    if (curr.size() == k)
-      output.add(new LinkedList(curr));
-
-    for (int i = first; i < n + 1; ++i) {
-      // add i into the current combination
-      curr.add(i);
-      // use next integers to complete the combination
-      backtrack(i + 1, curr);
-      // backtrack
-      curr.removeLast();
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> result = new LinkedList<>();
+        
+        generateCombinations(n, k, 1, new LinkedList<Integer>(), result);
+        
+        return result;
     }
-  }
-
-  public List<List<Integer>> combine(int n, int k) {
-    this.n = n;
-    this.k = k;
-    backtrack(1, new LinkedList<Integer>());
-    return output;
-  }
+    
+    private void generateCombinations(int n, int k, int index, LinkedList<Integer> currSet, 
+                                      List<List<Integer>> result){
+        
+        if(currSet.size() == k){
+            result.add(new LinkedList(currSet));
+        }
+        
+        for(int i = index; i <= n; ++i){
+            currSet.add(i);
+            generateCombinations(n, k, i + 1, currSet, result);
+            currSet.removeLast();
+        }
+    }
 }
