687af811dbd9b5a3f298cfac6c66ec619c57efc7
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-08-06 14:03:51 -0400

    Time: 24 ms (31.01%), Space: 222.6 MB (5.01%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        
+        List<List<String>> result = new ArrayList<>();
+        
+        findPalindromes(s, result, new LinkedList<String>(), 0);
+        
+        return result;
+    }
+    
+    private void findPalindromes(String s,
+                                List<List<String>> result, LinkedList<String> currList,
+                                int index){
+        
+        if(index == s.length()){
+            result.add(new LinkedList<>(currList));
+            return;
+        }
+        
+        for(int i = index; i < s.length(); i++){
+            
+            if(checkPalindrome(s, index, i)){
+                currList.add(s.substring(index, i + 1));
+                findPalindromes(s, result, currList, i + 1);
+                currList.removeLast();
+            }
+        }
+    }
+    
+    private boolean checkPalindrome(String s, int left, int right){
+        
+        while(left < right && right < s.length() && left < s.length()){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
