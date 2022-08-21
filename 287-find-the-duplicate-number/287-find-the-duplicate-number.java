1394fa394422cb9928cd4a8f2d144d623e935f18
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-08-21 14:23:28 -0400

    Time: 5 ms (87.17%), Space: 75 MB (81.30%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -4,11 +4,11 @@ class Solution {
         
         for(int i = 0; i < nums.length; i++){
             
-            if(nums[Math.abs(nums[i])] < 0){
+            if(nums[Math.abs(nums[i]) - 1] < 0){
                 return Math.abs(nums[i]);
             }
             
-            nums[Math.abs(nums[i])] *= -1;
+            nums[Math.abs(nums[i]) - 1] *= -1;
         }
         
         return -1;
