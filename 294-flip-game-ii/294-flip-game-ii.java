5328349dab290b1362001bfe53de0bd7ea101901
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2022-08-30 22:10:32 -0400

    Time: 25 ms (69.50%), Space: 51.6 MB (31.56%) - LeetHub

--- 294-flip-game-ii/294-flip-game-ii.java
+++ 294-flip-game-ii/294-flip-game-ii.java
@@ -5,10 +5,16 @@ class Solution {
     }
     
     public boolean canWin(String currentState) {
-        return canWin(new StringBuilder(currentState), new Boolean [currentState.length()], Player.PLAYER_1);
+        return canWin(new StringBuilder(currentState), new HashMap<String, Boolean>(), Player.PLAYER_1);
     }
     
-    private boolean canWin(StringBuilder currentState, Boolean [] memo, Player player){
+    private boolean canWin(StringBuilder currentState, Map<String, Boolean> memo, Player player){
+        
+        String key = currentState.toString();
+        
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
         
         for(int i = 0; i < currentState.length() - 1; i++){
 
@@ -17,16 +23,17 @@ class Solution {
             if(temp2.equals("++")){
                 
                 currentState.replace(i, i + 2, "--");
-
+                
                 if(!canWin(currentState, memo, player == Player.PLAYER_2 ? Player.PLAYER_1 : Player.PLAYER_2)){
                     currentState.replace(i, i + 2, temp2);
+                    memo.put(key, true);
                     return true;
                 }
-                
                 currentState.replace(i, i + 2, temp2);
             }
         }
         
-        return false;
+        memo.put(key, false);
+        return memo.get(key);
     }
 }
