52233e6a0088c59924e1f413242aef65cd846a27
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-07-23 18:09:25 -0400

    Time: 22 ms (9.23%), Space: 44.8 MB (5.83%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,56 @@
+class Solution {
+    
+    
+    public int evalRPN(String[] tokens) {
+        
+        Stack<Integer> operands = new Stack<>();
+        
+        for(String token: tokens){
+            
+            if(isDigit(token) || isNegative(token)){
+                if(isNegative(token)){
+                    int num = Integer.parseInt(token.substring(1));
+                    num *= -1;
+                    operands.push(num);
+                } else {
+                    int num = Integer.parseInt(token);
+                    operands.push(num);
+                }
+            } else if(token.equals("+") || token.equals("-") || token.equals("*")
+                     || token.equals("/")){
+                if(token.equals("+")){
+                    int num1 = operands.pop(), num2 = operands.pop();
+                    operands.push(num1 + num2);
+                } else if(token.equals("-")){
+                    int num1 = operands.pop(), num2 = operands.pop();
+                    operands.push(num2 - num1);
+                } else if(token.equals("*")){
+                    int num1 = operands.pop(), num2 = operands.pop();
+                    operands.push(num1 * num2);
+                } else if(token.equals("/")){
+                    int num1 = operands.pop(), num2 = operands.pop();
+                    operands.push(num2 / num1);
+                }
+            }
+        }
+        
+        int sum = 0;
+        
+        while(!operands.isEmpty()){
+            sum += operands.pop();
+        }
+        
+        return sum;
+    }
+    
+    
+    private boolean isNegative(String token){
+        return token.charAt(0) == '-' && token.length() > 1 && isDigit(token.substring(1));
+    }
+    
+    private boolean isDigit(String token){
+        char [] chars = token.toCharArray();
+        return IntStream.range(0, token.length()).map(i -> chars[i])
+            .allMatch(c -> Character.isDigit(c));
+    }
+}
