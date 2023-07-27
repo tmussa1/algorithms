856eaca3883ede0dffcd479d9142807c659a43e0
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-07-26 20:27:09 -0400

    Time: 1 ms (100.00%), Space: 39.6 MB (84.26%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public int mySqrt(int x) {
+        
+        if(x == 0) return x;
+        
+        int left = 0, right = x;
+        
+        while(left <= right){
+            
+            int mid = left + ((right - left) / 2);
+            
+            if(mid == 0) return 1;
+            
+            if(x / mid == mid){
+                return mid;
+            } else if(x / mid < mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return right;
+    }
+}
