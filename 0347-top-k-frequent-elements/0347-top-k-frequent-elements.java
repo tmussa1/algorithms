7c8ba44ab71134658198e75549a5f3db3db1b44c
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-08-04 20:57:37 -0400

    Time: 12 ms (86.75%), Space: 48.1 MB (9.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,32 +2,25 @@ class Solution {
     
     public int[] topKFrequent(int[] nums, int k) {
         
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        
+        Map<Integer, Integer> countMap = new HashMap<>();
         
         for(int num: nums){
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
         
-        PriorityQueue<Pair<Integer, Integer>> queue = new PriorityQueue<>((p1, p2) -> p1.getValue() - p2.getValue());
-        
-        
-        for(Map.Entry<Integer, Integer> entry: frequencyMap.entrySet()){
-            
-            if(queue.size() < k){
-               queue.offer(new Pair<>(entry.getKey(), entry.getValue()));
-            } else if(queue.peek().getValue() < entry.getValue()) {
-               queue.poll();
-               queue.offer(new Pair<>(entry.getKey(), entry.getValue()));
-            }
+        for(Map.Entry<Integer, Integer> entry: countMap.entrySet()){
+            queue.offer(entry);
         }
-               
         
-        int [] result = new int[k];
         int index = 0;
         
-        while(!queue.isEmpty()){
-            result[index] = queue.poll().getKey();
-            index++;
+        int [] result = new int[k];
+        
+        while(!queue.isEmpty() && index < k){
+            result[index++] = queue.poll().getKey();
         }
         
         return result;
