1d5868dafddf2a33aa2debcf35d8628bb129e305
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-08-10 17:29:17 -0400

    Time: 12 ms (5.62%), Space: 41.6 MB (6.92%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,38 @@
+class Solution {
+    
+    public int numDecodings(String s) {
+        return canDecod(new HashMap<>(), s, 0);
+    }
+    
+    
+    private int canDecod(Map<Integer, Integer> memo, String s, int index){
+        
+        
+        if(index == s.length()) return 1;
+        
+        if(memo.containsKey(index)) return memo.get(index);
+            
+        int ways = 0;
+        
+        if(index + 1 <= s.length() && isDecodable(s.substring(index, index + 1))){
+            System.out.println("index 1 " + s.substring(index, index + 1));
+            ways += canDecod(memo, s, index + 1);
+        } 
+        
+        if(index + 2 <= s.length() && isDecodable(s.substring(index, index + 2))){
+            System.out.println("index 2 " + s.substring(index, index + 2));
+            ways += canDecod(memo, s, index + 2);
+        } 
+        
+        memo.put(index, ways);
+        
+        return memo.get(index);
+    }
+    
+    
+    private boolean isDecodable(String letter){
+        return letter.length() <= 2 && letter.charAt(0) != '0'
+            && Integer.parseInt(letter) <= 26
+            && Integer.parseInt(letter) >= 1;
+    }
+}
