711d63d95d3d5bf31a3130fc38befdd3941bf4cb
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-08-25 19:39:56 -0400

    Time: 1 ms (25.73%), Space: 49.6 MB (84.34%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    
+    public int singleNonDuplicate(int[] nums) {
+        
+        int xor = 0;
+        
+        for(int num: nums){
+            xor ^= num;
+        }
+        
+        return xor;
+    }
+}
