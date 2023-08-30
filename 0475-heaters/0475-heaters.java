6984e51edfc95724ee13b49011790d5c66c0fec0
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-08-29 21:46:27 -0400

    Time: 13 ms (79.64%), Space: 46 MB (84.83%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -14,16 +14,11 @@ class Solution {
             while(heaterIndex < heaters.length){
                 prevDiff = diff;
                 diff = Math.abs(houses[houseIndex] - heaters[heaterIndex]);
-                // System.out.println("Each " + prevDiff + " diff " + diff);
                 if(diff > prevDiff) break;
                 heaterIndex++;
             }
             
             if(diff > prevDiff || heaterIndex == heaters.length) heaterIndex--;
-            
-            // System.out.println("House " + houses[houseIndex]);
-            // System.out.println("Heater " + heaterIndex);
-            // System.out.println("Diff " + prevDiff);
             
             radius = Math.max(radius, Math.min(diff, prevDiff));
             
