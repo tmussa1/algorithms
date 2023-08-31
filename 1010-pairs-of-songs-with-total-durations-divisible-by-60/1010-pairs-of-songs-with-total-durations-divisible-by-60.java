3788f57844e0b7b22d4fe0ae9519616f583509bc
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-08-31 17:15:59 -0400

    Time: 21 ms (29.35%), Space: 50.2 MB (55.10%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -11,9 +11,8 @@ class Solution {
             int key = time[i] % 60;
             if(indexMap.containsKey(60 - key)){
                 count += indexMap.get(60 - key);
-                System.out.println(key + " key " + indexMap.get(60 - key) + " I " + i);
             } else if(key == 0){
-                  count += indexMap.get(0);
+                count += indexMap.get(0);
             }
             indexMap.put(key, indexMap.getOrDefault(key, 0) + 1);
         }
