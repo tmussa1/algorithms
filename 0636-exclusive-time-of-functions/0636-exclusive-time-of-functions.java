784fdc11c6feff9080fbc3268447d4aa3e4a5f5b
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-09-13 21:11:32 -0400

    Time: 12 ms (61.51%), Space: 44.5 MB (40.26%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.java
@@ -17,16 +17,12 @@ class Solution {
             if(logSplit[1].equals("start")){
                 if(!timeStack.isEmpty()){
                     int [] last = timeStack.peek();
-                    System.out.println("Before peek result " + Arrays.toString(result));
                     result[last[0]] += (Integer.parseInt(logSplit[2]) - end - 1);
-                    System.out.println("After peek result " + Arrays.toString(result));
                 } 
                 timeStack.push(new int [] {Integer.parseInt(logSplit[0]), Integer.parseInt(logSplit[2])});
             } else {
                   int [] last = timeStack.pop();
-                  System.out.println("Before pop result " + Arrays.toString(result));
                   result[last[0]] += (Integer.parseInt(logSplit[2]) - Math.max(last[1], end) + 1);
-                  System.out.println("After pop result " + Arrays.toString(result));
             }
              end = Integer.parseInt(logSplit[2]);
         }
