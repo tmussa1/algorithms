ab0adbeab07c5e7acea833c86e7b583af7e7aeb3
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-09-23 17:58:48 -0400

    Time: 0 ms (100.00%), Space: 44.2 MB (77.12%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
@@ -9,13 +9,15 @@ class Solution {
         int [] [] result = new int [mat1Row][mat2Col];
         
         for(int row = 0; row < mat1Row; row++){
-            
           for(int row2 = 0; row2 < commonCol; row2++){
+              
+              if(mat1[row][row2] != 0){
                 for(int col = 0; col < mat2Col; col++){
                     int val1 = mat1[row][row2];
                     int val2 = mat2[row2][col];
                     result[row][col] += (val1 * val2);
                 }
+              }
            }
         }
         
