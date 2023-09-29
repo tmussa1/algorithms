afc3804c8175bb788b2206419fbe769a7c14ab0d
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-09-29 15:20:41 -0400

    Time: 6 ms (21.15%), Space: 44.9 MB (42.90%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.java
@@ -0,0 +1,58 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    
+    public int getImportance(List<Employee> employees, int id) {
+        
+        Map<Integer, Integer> importanceMap = new HashMap<>();
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        
+        buildAdjList(importanceMap, adjList, employees);
+        
+        // System.out.println(adjList);
+        // System.out.println(importanceMap);
+        
+        return dfs(adjList, importanceMap, id, new HashSet<>());
+    }
+    
+    private int dfs(Map<Integer, List<Integer>> adjList, 
+                    Map<Integer, Integer> importanceMap,
+                    int id, Set<Integer> visited){
+        
+        // System.out.println(id);
+        
+        if(visited.contains(id)){
+            return 0;
+        }
+        
+        visited.add(id);
+        
+        int result = importanceMap.get(id);
+        
+        for(Integer neighbor: adjList.get(id)){
+            if(!visited.contains(neighbor)){
+                result += dfs(adjList, importanceMap, neighbor, visited);
+            }
+        }
+        
+        return result;
+    }
+    
+    private void buildAdjList(Map<Integer, Integer> importanceMap,
+                              Map<Integer, List<Integer>> adjList,
+                              List<Employee> employees){
+        
+        for(Employee employee: employees){
+            adjList.put(employee.id, employee.subordinates);
+            importanceMap.put(employee.id, employee.importance);
+        }
+        
+    }
+}
