e843d672de95d88d87bca11dbcc8fb50d1219aa4
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-09-29 19:53:20 -0400

    Time: 496 ms (14.90%), Space: 43.7 MB (75.17%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.java
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.java
@@ -35,24 +35,6 @@ class Solution {
                     int right = pair1.getValue() - pair4.getValue();
                     int left = pair3.getValue() - pair2.getValue();
                     
-//                     int diagonal1 = ((int) (Math.pow(Math.abs(pair3.getKey() - pair1.getKey()), 2) +
-//                         Math.pow(Math.abs(pair3.getValue() - pair1.getValue()), 2)));
-                    
-//                     int diagonal2 = ((int) (Math.pow(Math.abs(pair4.getKey() - pair2.getKey()), 2) +
-//                         Math.pow(Math.abs(pair4.getValue() - pair2.getValue()), 2)));
-                    
-//                     int diagonal3 = ((int) (Math.pow(Math.abs(pair3.getKey() - pair2.getKey()), 2) +
-//                         Math.pow(Math.abs(pair3.getValue() - pair2.getValue()), 2)));
-                    
-//                     int diagonal4 = ((int) (Math.pow(Math.abs(pair4.getKey() - pair3.getKey()), 2) +
-//                         Math.pow(Math.abs(pair4.getValue() - pair3.getValue()), 2)));
-                    
-//                      System.out.println("Result " + xDiff + " " + yDiff + " diag " + diagonal1);
-//                      System.out.println("Pair 1 " + pair1);
-//                      System.out.println("Pair 2 " + pair2);
-//                      System.out.println("Pair 3 " + pair3);
-//                      System.out.println("Pair 4 " + pair4);
-                    
                     if(top != bottom || left != right) {
                         continue;
                     }
