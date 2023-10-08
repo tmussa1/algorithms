f659c2891a6168df88fdd9d2e594382cde5292c3
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-08 16:05:49 -0400

    Time: 332 ms (5.21%), Space: 98.6 MB (5.00%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        
+        char [] sArr = s.toCharArray();
+        
+        return minFlipsMonoIncr(sArr, 0, s.length(), true, new HashMap<>());
+    }
+    
+    private int minFlipsMonoIncr(char [] sArr, int index, int length, boolean isZero,
+                                 Map<String, Integer> memo){
+        
+        String key = isZero + "," + index;
+        
+        if(index == length){
+            return 0;
+        }
+        
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        
+        int result = Integer.MAX_VALUE;
+        
+        char c = sArr[index];
+        
+        if(isZero){
+            if(c == '0'){
+                return minFlipsMonoIncr(sArr, index + 1, length, isZero, memo);
+            } else {
+                sArr[index] = '0';
+                int changed = 1 + minFlipsMonoIncr(sArr, index + 1, length, isZero, memo);
+                sArr[index] = '1';
+                int unchanged = minFlipsMonoIncr(sArr, index + 1, length, !isZero, memo);
+                result = Math.min(result, Math.min(changed, unchanged));
+            }
+        } else {
+            if(c == '1'){
+                return minFlipsMonoIncr(sArr, index + 1, length, isZero, memo);
+            } else {
+                sArr[index] = '1';
+                int changed = 1 + minFlipsMonoIncr(sArr, index + 1, length, isZero, memo);
+                sArr[index] = '0';
+                result = Math.min(result, changed);
+            }
+        }
+        
+        memo.put(key, result);
+        
+        
+        return result;
+    }
+}
