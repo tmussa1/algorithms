233e2b723558305fb6ac2884dd45713c0e02e2b6
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-10 16:42:20 -0400

    Time: 1 ms (79.80%), Space: 40.1 MB (86.73%) - LeetHub

---
+++ 0388-longest-absolute-file-path/0388-longest-absolute-file-path.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        
+        String [] inputSplit = input.split("\n");
+        
+        int countOfWords = 0, length = 0, result = 0;
+        
+        Stack<String> stack = new Stack<>();
+        Stack<Integer> tabCount = new Stack<>(); 
+        
+        for(String word: inputSplit){
+           int index = word.lastIndexOf("\t");
+           word = word.substring(Math.max(0, index + 1));
+         //  System.out.println(word);
+           if(tabCount.isEmpty() || index > tabCount.peek()){
+               stack.push(word);
+               countOfWords++;
+               length += word.length();
+               tabCount.push(index);
+               if(word.indexOf(".") != -1){
+                   result = Math.max(length + countOfWords - 1, result);
+                   countOfWords--;
+                   length -= word.length();
+                   stack.pop();
+                   tabCount.pop();
+               }
+           } else {
+               while(!tabCount.isEmpty() && index <= tabCount.peek()){
+                   tabCount.pop();
+                   String word1 = stack.pop();
+                   countOfWords--;
+                   length -= word1.length();
+               }
+               stack.push(word);
+               countOfWords++;
+               length += word.length();
+               tabCount.push(index);
+               if(word.indexOf(".") != -1){
+                   result = Math.max(length + countOfWords - 1, result);
+                   countOfWords--;
+                   length -= word.length();
+                   stack.pop();
+                   tabCount.pop();
+               }
+           }
+        //   System.out.println(stack);
+        //   System.out.println(tabCount);
+        }
+            
+        return result;
+    }
+}
