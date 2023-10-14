e4859c7021b2e566a8efc31985e61f8aa253d790
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-14 15:14:26 -0400

    Time: 0 ms (100.00%), Space: 43.2 MB (48.61%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,72 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int length = mountainArr.length();
+        
+        int left = 0, right = length - 1, maxIndex = 0;
+        
+        while(left <= right) {
+            
+            int mid = left + ((right - left) / 2);
+            
+            int val = mountainArr.get(mid);
+            
+            if(mid - 1 >= 0 && mountainArr.get(mid - 1) < val && mid + 1 < length && val > mountainArr.get(mid + 1)){
+                maxIndex = mid;
+                break;
+            } else if(mid - 1 >= 0 && mountainArr.get(mid - 1) > val){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        left = 0;
+        right = maxIndex;
+        
+        while(left <= right) {
+            
+            int mid = left + ((right - left) / 2);
+            
+            int val = mountainArr.get(mid);
+            
+            if(val == target){
+               return mid;
+            } else if(val < target){
+               left = mid + 1;
+            } else {
+               right = mid - 1;
+            }
+        }
+        
+        left = maxIndex + 1;
+        right = length - 1;
+        
+        while(left <= right) {
+            
+            int mid = left + ((right - left) / 2);
+            
+            int val = mountainArr.get(mid);
+            
+            if(val == target){
+               return mid;
+            } else if(val < target){
+               right = mid - 1;
+            } else {
+               left = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
