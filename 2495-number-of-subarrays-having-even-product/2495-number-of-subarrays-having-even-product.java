aaa31adcc0a542c069751e66a638d1ac23a65abc
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-16 15:34:48 -0400

    Time: 6 ms (19.05%), Space: 56.7 MB (95.24%) - LeetHub

---
+++ 2495-number-of-subarrays-having-even-product/2495-number-of-subarrays-having-even-product.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public long evenProduct(int[] nums) {
+        
+        long sum = 0;
+        
+        long [] dp = new long [nums.length];
+        
+        long max = 0;
+        
+        int lastEvenIndex = -1;
+        
+        for(int index = 0; index < nums.length; index++){
+
+            if(nums[index] % 2 == 0){
+                lastEvenIndex = index;
+            }
+            
+            dp[index] += Math.max(lastEvenIndex + 1, 0);
+
+            max += dp[index];
+        }
+        
+     //   System.out.println(Arrays.toString(dp));
+        
+        return max;
+    }
+}
