7bbe802538e7eec1db692e65b5ee561aec79183a
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-17 22:25:35 -0400

    Time: 4 ms (12.68%), Space: 40.3 MB (62.13%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -4,13 +4,9 @@ class Solution {
         
         Arrays.sort(citations);
         
-    //    if(citations[0] > citations.length) return citations[0] == 0 ? 0 : citations.length;
-        
         int length = citations.length;
         
         int left = 0, right = Arrays.stream(citations).sum(), result = 0;
-        
-  //      System.out.println("Arr " + Arrays.toString(citations));
         
         while(left <= right){
             
@@ -18,13 +14,10 @@ class Solution {
             
             int index = findIndex(citations, mid);
             
-            int val = length - index;
+            int citesRight = length - index;
             
-       //     System.out.println("Mid " + mid + " index " + index + " val " + val);
-            
-            if(val >= mid){
+            if(citesRight >= mid){
                 result = Math.max(result, mid);
-        //        System.out.println("Res " + result);
                 left = mid + 1;
             } else {
                 right = mid - 1;
@@ -41,8 +34,6 @@ class Solution {
         while(left <= right){
             
             int mid = left + ((right - left) / 2);
-            
-       //     System.out.println("MId here " + mid + " val " + val);
             
             if(citations[mid] == val){
                 result = Math.min(result, mid);
