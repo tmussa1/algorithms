2e7389893f8ed05b1008f2ec59b399682fb4cc01
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-28 15:47:16 -0400

    Time: 4 ms (100.00%), Space: 41.4 MB (91.43%) - LeetHub

---
+++ 1058-minimize-rounding-error-to-meet-target/1058-minimize-rounding-error-to-meet-target.java
@@ -0,0 +1,86 @@
+class Solution {
+    
+    public String minimizeError(String[] pricesStr, int target) {
+        
+        double [] prices = new double[pricesStr.length];
+        
+        int floorSum = 0, ceilSum = 0;
+        
+        for(int i = 0; i < pricesStr.length; i++){
+            prices[i] = Double.parseDouble(pricesStr[i]);
+            floorSum += Math.floor(prices[i]);
+            ceilSum += Math.ceil(prices[i]);
+        }
+        
+        if(ceilSum < target || target < floorSum) return "-1";
+        
+        int diff = 0;
+        
+      //  Arrays.sort(prices);
+        
+    //    System.out.println(Arrays.toString(prices));
+        
+        PriorityQueue<Double> queue = new PriorityQueue<Double>((a, b) -> {
+            
+            double aDiff = a - Math.floor(a);
+            double bDiff = b - Math.floor(b);
+            
+            if(Double.compare(aDiff, bDiff) == 0){
+                return Double.compare(b, a);
+            }
+            
+            return Double.compare(bDiff, aDiff);
+        });
+        
+        for(double price: prices){
+            queue.offer(price);
+        }
+        
+    //    System.out.println(queue);
+        
+        double result = 0.0;
+        int sum = 0;
+        
+        while(!queue.isEmpty()){
+            
+            double curr = queue.poll();
+            
+        //    System.out.println("Curr "+ curr);
+            
+            if(diff < target - floorSum){
+                sum += Math.ceil(curr);
+                diff += 1;
+                result += (Math.ceil(curr) - curr);
+            } else {
+                sum += Math.floor(curr);
+                result += (curr - Math.floor(curr));
+            }
+        }
+        
+        double res = Math.round(result * 1000.0) / 1000.0;
+        
+        String value = String.valueOf(res);
+        
+        int index = value.lastIndexOf(".");
+        int firstIndex = index;
+        int length = value.length();
+        int dif = 0;
+        
+        while(index < value.length() && value.charAt(index) != '0'){
+            index++;
+        }
+        
+     //   System.out.println("Index " + index + " " + firstIndex);
+        
+        diff += (index - firstIndex);
+        
+        while(dif < 2){
+            value += "0";
+            dif++;
+        }
+        
+        int index1 = value.lastIndexOf(".");
+        
+        return value.substring(0, index1 + 4);
+    }
+}
