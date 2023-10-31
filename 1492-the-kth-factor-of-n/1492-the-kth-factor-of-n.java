31d34c5062d3f42ee26396b4482edaa99586436a
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2023-10-31 17:47:15 -0400

    Time: 93 ms (5.00%), Space: 43.2 MB (5.10%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    
+    public int kthFactor(int n, int k) {
+        
+        if(n <= 1) return n;
+        
+        TreeSet<Integer> factors = new TreeSet<>();
+        factors.add(1);
+        
+        for(int i = 2; i <= n; i++){
+            
+            if(n % i == 0 && !factors.contains(i)){
+                
+                int num = i;
+                
+                while(num <= n){
+                    if(n % num == 0){
+                       factors.add(num);
+                    }
+                    num *= 2;
+                }
+                
+            }
+            
+            System.out.println(factors);
+            
+            NavigableSet<Integer> facts = factors.headSet(i, true);
+            
+            if(facts.size() >= k){
+                
+                Iterator<Integer> iter = facts.iterator();
+                
+                int count = 0;
+                
+                while(count < k && iter.hasNext()){
+                    count++;
+                    int val = iter.next();
+                    
+                    if(count == k){
+                        return val;
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
